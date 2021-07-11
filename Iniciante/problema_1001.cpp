e332c3fb18decd397b4d35dabf25da8b65187aa4
jescascela <jefferson.escascela@gmail.com>
2021-07-11 20:23:29 -0300

    Problema 1001

---
+++ Iniciante/problema_1001.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a;
+    int b;
+    int x;
+
+    cin >> a;
+    cin >> b;
+
+    x = a + b;
+
+    cout << "X = " << x << endl;
+
+    return 0;
+}
